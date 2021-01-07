b3395c55d40d79cbc4ecffc41e522751c67688fc
﻿Adam Urish <adamurish@gmail.com>
2021-01-07 16:13:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from mnist_example.mnist_convnet import prepare_data, build_model, train_model, 
 (x_train, y_train), (x_test, y_test) = prepare_data()
 model = build_model()
 trained_model = train_model(model, x_train, y_train)
-evaluate_model(trained_model)
+evaluate_model(trained_model, x_test, y_test)
