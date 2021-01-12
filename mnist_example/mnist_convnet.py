f9acbe48ad9d1a48f6a4e51d2f230bde3eb3a2ca
﻿Adam Urish <adamurish@gmail.com>
2021-01-12 16:45:04 -0600

    Update mnist_convnet.py

--- mnist_example/mnist_convnet.py
+++ mnist_example/mnist_convnet.py
@@ -84,3 +84,4 @@ def evaluate_model(model, x_test, y_test):
     score = model.evaluate(x_test, y_test, verbose=0)
     print("Test loss:", score[0])
     print("Test accuracy:", score[1])
+    return score
