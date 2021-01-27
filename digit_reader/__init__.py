c77d566e653db5279389cd6b9e4bebd47d466b6e
adamwu2 <adamwu2@illinois.edu>
2021-01-27 13:17:00 -0600

    seperate code into respective files

--- digit_reader/__init__.py
+++ digit_reader/__init__.py
@@ -1,6 +1,1 @@
-"""A simple package containing some example keras code
-
-There are functions to build, train, and evaluate a basic
-convolutional model to classify images from the MNIST dataset.
-
-"""
+"""A simple package containing some example keras code"""
---
+++ digit_reader/model/__init__.py
@@ -0,0 +1,7 @@
+"""Basic Keras model for MNIST
+
+Runs on the MNIST handwritten number dataset.
+Achieves >90% accuracy with few epochs, making it great for
+an example project.
+
+"""
