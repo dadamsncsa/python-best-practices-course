115de4d1c51901640115d7e5425803831f31ee83
adamwu2 <adamwu2@illinois.edu>
2021-01-28 10:20:21 -0600

    add src dir and update tests

--- tests/mnist_test.py
+++ tests/mnist_test.py
@@ -1,4 +1,5 @@
-from mnist_example.mnist_convnet import prepare_data, build_model, train_model, evaluate_model
+from digit_reader.model.helpers import prepare_data
+from digit_reader.model.model import MNISTModel
 
 def test_failing():
     # this test should initially be failing for some reason, then the user will fix it
@@ -14,30 +15,26 @@ def test_x_shape():
     assert x_test.shape[0] == 10000
     # test if images are square
     assert x_train.shape[1] == x_train.shape[2]
-    
+
 
 def test_y_shape():
     (x_train, y_train), (x_test, y_test) = prepare_data()
     # test if all training labels were loaded
     assert y_train.shape[0] == 60000
-    # test if all test labels were loaded 
+    # test if all test labels were loaded
     assert y_test.shape[0] == 10000
 
 
 def test_model_built():
-    model = build_model()
-    # test if model weights were generated
-    assert model.get_weights() is not None
-    # test if each of the layer connections has weights
-    # there are 7 layers, so 6 weights
-    assert len(model.get_weights()) == 6
+    model = MNISTModel()
+    # test if model was generated
+    assert model is not None
 
 
 def test_model_score():
     (x_train, y_train), (x_test, y_test) = prepare_data()
-    model = build_model()
-    trained_model = train_model(model, x_train, y_train)
-    score = evaluate_model(trained_model, x_test, y_test)
+    model = MNISTModel()
+    model.train_model(x_train, y_train, epochs=2)
+    score = evaluate_model(x_test, y_test)
     # test if accuracy was greater than 90%
     assert score[1] >= .9
-
