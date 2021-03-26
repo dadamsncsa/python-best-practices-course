88dcec0d382889e64876b66eff075128c6b19389
Darren Adams <dadamsncsa@gmail.com>
2021-03-25 20:04:26 -0500

    added more file tests

--- mnist_example/test/test_license.py
+++ mnist_example/test/test_license.py
@@ -4,3 +4,9 @@ import pytest
 class TestForFiles():
     def test_for_license(self):
         assert os.path.isfile("LICENSE")
+
+    def test_for_gitignore(self):
+        assert os.path.isfile(".gitignore")
+    
+    def test_for_readme(self):
+        assert os.path.isfile("Readme.md")
