ae36bdb6dfb9c7b122c131d1302766301a0de4b5
briancy02 <64820854+briancy02@users.noreply.github.com>
2021-04-26 05:23:17 +0900

    Update test_initial_files.py

--- tests/course/test_initial_files.py
+++ tests/course/test_initial_files.py
@@ -1,1 +1,12 @@
- 
+import os
+import pytest
+
+class TestForFiles():
+    def test_for_license(self):
+        assert os.path.isfile("LICENSE")
+
+    def test_for_gitignore(self):
+        assert os.path.isfile(".gitignore")
+    
+    def test_for_readme(self):
+        assert os.path.isfile("README.md") 
