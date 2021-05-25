2a62899f0581207237f58e5a11d1f6f63b0c3d36
Darren Adams <dadams@illinois.edu>
2021-05-25 13:53:53 -0500

    Update model.py

--- src/digit_reader/model/model.py
+++ src/digit_reader/model/model.py
@@ -2,6 +2,12 @@ class MNISTModel:
     def __init__(self):
         pass
     def train_model(self):
+        """
+        some notes
+        """
         pass
-    def evaluate_model(self):
+    def evaluate_model(self
+        """
+        more notes
+        """
         pass
